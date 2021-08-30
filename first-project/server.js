e96e1dc9912ee34f9c79e93e5622ab6fecc5a91c
Ron Schmitt <ron.schmitt@protonmail.com>
2021-08-30 11:49:21 -0400

    thru chap 35

---
+++ first-project/server.js
@@ -0,0 +1,9 @@
+console.log('My First App Node Server Started');
+
+const http = require('http');
+const server = http.createServer( (req, res) => {
+  res.end('This my first response');
+} );
+
+server.listen( process.env.PORT || 3000 );
+
