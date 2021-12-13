30e25b1f188a249d38d88260ecdbe8f4ec38bf77
Ron Schmitt <ron.schmit@caa.columbia.edu>
2021-12-13 14:06:55 -0500

    lectures 69 and 71

--- first-project/app.js
+++ first-project/app.js
@@ -8,7 +8,6 @@ const chalk = require('chalk');
 const config = require('./config.js');
 // console.log(`app.js: config.INTEGRATE_ANGULAR=${config.INTEGRATE_ANGULAR}`);
 
-
 // ---- Mongoose ----
 const mongoose = require('mongoose');
 
@@ -33,8 +32,6 @@ async function main() {
   await mongoose.connect(`mongodb+srv://main:${password}@cluster0.ukfax.mongodb.net/mean-course?retryWrites=true&w=majority`);
 }
 
-// This is the mongoose/mongoDB data model.  It is a class with a constructor
-const Post = require('./postDB');
 
 
 
@@ -72,84 +69,17 @@ app.use((req, res, next) => {
   next();
 });
 
-// create a new post in the db
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  })
-  console.log(`app.js: post request received route=${req.route.path}`, post);
-  console.log(req.body);
-  post.save().then(result => {
-    res.status(201).json({
-      message: 'New post added successfully',
-      id: result._id,
-    });
-  });
-});
-
-// update a post given by id
-// ':id' implies that id is sent in req.params not req.body
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  console.log(`app.js: put request received id=${req.params.id} route=${req.route.path}`, post);
-  Post.updateOne({ _id: req.params.id }, post).then(result => {
-    console.log(`app.js: put /api/posts/:id: updated post on MongoDb `, result);
-    res.status(200).json({ message: 'Update Sucessful!' });
-  })
-});
-
-// get the posts database
-app.get('/api/posts', (req, res, next) => {
-  // retrieve exisiting posts from MongoDB
-  Post.find().then(documents => {
-    // the data items in MongoDB are called documents
-    console.log(documents);
-    // retrieve is asynchronous, so we can't process until the callback gets called
-    // send status, then send posts as a JSON message!
-    res.status(200).json({
-      message: 'Posts fetched successfully!',
-      posts: documents,
-    });
-  });
-});
-
-// update a post given by id
-// ':id' implies that id is sent in req.params not req.body
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({
-        message: 'Post not found!',
-      });
-    }
-  });
-});
-
-// delete a post given by id
-// ':id' implies that id is sent in req.params not req.body
-app.delete("/api/posts/:id", (req, res, next) => {
-  console.log(`app.js: delete request received id=${req.params.id}`);
-  Post.deleteOne({ _id: req.params.id })
-    .then(result => {
-      // console.log(result);
-      res.status(200).json({ message: "Post deleted!" });
-    });
-});
-
-
 if (config.INTEGRATE_ANGULAR) {
   app.use('/api/posts', (req, res, next) => {
-    const angIndex = path.join(__dirname, "angular", "index.html");
-    console.log(`config.js: sending Angular FE: ${angIndex}`);
-    res.sendFile(angIndex);
+      const angIndex = path.join(__dirname, "angular", "index.html");
+      console.log(`config.js: sending Angular FE: ${angIndex}`);
+      res.sendFile(angIndex);
   });
 }
+
+
+// routes
+const postsRoutes = require('./posts');
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
--- first-project/src/app/posts/posts.service.ts
+++ first-project/src/app/posts/posts.service.ts
@@ -14,7 +14,7 @@ export class PostsService {
 
   getPosts() {
     // get posts from back-end using http request!
-    const url = environment.apiUrl+'/posts';
+    const url = environment.apiUrl + '/posts';
     console.log(`send get message to : `, url);
     return this.http.get<{ message: string, posts: any }>(url)
       .pipe(
@@ -40,13 +40,13 @@ export class PostsService {
 
   getPost(id: string) {
     // return an observable that consumer can subscribe to   
-    return this.http.get<{ _id: string, title: string, content: string, }>(environment.apiUrl+'/posts/'+id);
+    return this.http.get<{ _id: string, title: string, content: string, }>(environment.apiUrl + '/posts/' + id);
   }
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title, content };
-    console.warn(`addPost: post:`,post)
-    this.http.post<{ message: string, id: string }>(environment.apiUrl+'/posts', post)
+    console.warn(`addPost: post:`, post)
+    this.http.post<{ message: string, id: string }>(environment.apiUrl + '/posts', post)
       .subscribe((response) => {
         console.log(`add response received: `, response.message);
         post.id = response.id;
@@ -58,23 +58,23 @@ export class PostsService {
 
   updatePost(postId: string, title: string, content: string) {
     const post: Post = { id: postId, title, content };
-    this.http.put(environment.apiUrl+'/posts/'+postId, post)
-    .subscribe((response) => {
-      console.log(`put response received: `, response);
-      // update local copy. not needed at this point, but do it anyway
-      const updatedPosts = [...this.posts];
-      const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
-      updatedPosts[oldPostIndex] = post;
-      this.posts = updatedPosts;
-      this.postsObserved.next([...this.posts]);
-    });
-    
+    this.http.put(environment.apiUrl + '/posts/' + postId, post)
+      .subscribe((response) => {
+        console.log(`put response received: `, response);
+        // update local copy. not needed at this point, but do it anyway
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsObserved.next([...this.posts]);
+      });
+
   }
 
   deletePost(postId: string) {
     // delete from MongoDB
-    console.log(`delete button pressed id=${postId} path=${environment.apiUrl+'/posts/'+postId}`);
-    this.http.delete(environment.apiUrl+'/posts/'+postId)
+    console.log(`delete button pressed id=${postId} path=${environment.apiUrl + '/posts/' + postId}`);
+    this.http.delete(environment.apiUrl + '/posts/' + postId)
       .subscribe(() => {
         // delete from our local copy
         this.posts = this.posts.filter(post => post.id !== postId);
