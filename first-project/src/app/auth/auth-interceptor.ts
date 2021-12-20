dbfdd3435ba1cc8624e0714ef87e4f745ee86da0
Ron Schmitt <ron.schmit@caa.columbia.edu>
2021-12-20 13:05:46 -0500

    lecture 115

--- first-project/src/app/auth/auth-interceptor.ts
+++ first-project/src/app/auth/auth-interceptor.ts
@@ -2,6 +2,10 @@ import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http"
 import { Injectable } from "@angular/core";
 import { AuthService } from "./auth.service";
 
+// AuthInterceptor inspects HTTP requests to/from your application to the server
+// It is entered as a provider in app.module.ts.
+// https://angular.io/guide/http#intercepting-requests-and-responses
+
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor{
 
@@ -10,6 +14,7 @@ export class AuthInterceptor implements HttpInterceptor{
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
+        // insert our authToken on the HTTP request so that it gets passed to the backend
         const authToken = this.authService.getToken();
         const authRequest = req.clone({
             headers: req.headers.set("Authorization", "Bearer "+ authToken),
--- first-project/src/app/auth/auth.guard.ts
+++ first-project/src/app/auth/auth.guard.ts
@@ -3,6 +3,11 @@ import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTr
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
+
+// The AuthGuard is used by app-routing.module.ts and protects hidden content when user
+// is not logged in.
+// https://angular.io/guide/router#milestone-5-route-guards
+
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(
@@ -16,6 +21,7 @@ export class AuthGuard implements CanActivate {
     ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
         const isAuth = this.authService.getIsAuth();
         if (!isAuth) {
+            // is route is blocked, direct user to login
             this.router.navigate(['/login']);
         }
         return true;
