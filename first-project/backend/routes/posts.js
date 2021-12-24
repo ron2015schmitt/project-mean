f008c3f9796f352114f064cbc3a52418bfe205cf
Ron Schmitt <ron.schmit@caa.columbia.edu>
2021-12-24 17:11:47 -0500

    lecture 131

--- first-project/backend/routes/posts.js
+++ first-project/backend/routes/posts.js
@@ -4,51 +4,22 @@ const checkAuth = require("../middleware/check-auth");
 
 const PostController = require('../controllers/posts');
 
-// use multer for image transfers
-const multer = require("multer");
-const MIME_TYPE_MAP = {
-    "image/png": "png",
-    "image/jpeg": "jpg",
-    "image/jpg": "jpg"
-};
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const isValid = MIME_TYPE_MAP[file.mimetype];
-        let error = new Error("Invalid mime type");
-        if (isValid) {
-            error = null;
-        }
-        cb(error, "backend/images");
-    },
-    filename: (req, file, cb) => {
-        const name = file.originalname
-            .toLowerCase()
-            .split(" ")
-            .join("-");
-        const ext = MIME_TYPE_MAP[file.mimetype];
-        cb(null, name + "-" + Date.now() + "." + ext);
-    }
-});
+// multer is set up to extract an image file from the field "image", if present
+const multer = require("../middleware/multer");
 
 
 // create a new post in the db
 router.post(
     "",
     checkAuth,
-    multer({ storage: storage }).single("image"),  // grabs field named "image"
+    multer, // grabs field named "image"
     PostController.createPost,
 );
 
 
 // update a post given by id
 // ':id' implies that id is sent in req.params not req.body
-router.put(
-    '/:id',
-    multer({ storage: storage }).single("image"), // grabs field named "image"
-    checkAuth,
-    PostController.updatePost,
-);
+router.put('/:id', multer, checkAuth, PostController.updatePost);
 
 // get the posts database
 router.get('', PostController.getPosts );
